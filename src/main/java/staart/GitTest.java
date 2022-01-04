3976bf8f925b8c6dad13972d5b99722c2076c2d5
Hznstart <2329895525@qq.com>
2022-01-05 01:32:18 +0800

    Initial commit

---
+++ src/main/java/staart/GitTest.java
@@ -0,0 +1,12 @@
+package staart;
+
+/**
+ * @author znstart
+ * @date 2022/1/5-0:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+        System.out.println("hello Git 2");
+    }
+}
